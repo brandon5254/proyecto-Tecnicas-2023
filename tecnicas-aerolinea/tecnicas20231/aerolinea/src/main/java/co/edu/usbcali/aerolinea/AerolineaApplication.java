d62109d8484f0c32e7bd2b1d92b02750d811651c
brandon5254 <83289428+brandon5254@users.noreply.github.com>
2023-11-15 20:58:41 -0500

    .

--- tecnicas-aerolinea/tecnicas20231/aerolinea/src/main/java/co/edu/usbcali/aerolinea/AerolineaApplication.java
+++ tecnicas-aerolinea/tecnicas20231/aerolinea/src/main/java/co/edu/usbcali/aerolinea/AerolineaApplication.java
@@ -2,12 +2,19 @@ package co.edu.usbcali.aerolinea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class AerolineaApplication {
+public class AerolineaApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AerolineaApplication.class, args);
 	}
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AerolineaApplication.class);
+	}
 }
